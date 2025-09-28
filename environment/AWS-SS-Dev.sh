3a1927b11136c2fea86b94a30888b9a4b96b602f
manojxg <87294078+manojxg@users.noreply.github.com>
2025-09-28 19:50:13 +0530

    Update AWS-SS-Dev.sh

--- environment/AWS-SS-Dev.sh
+++ environment/AWS-SS-Dev.sh
@@ -1,4 +1,4 @@
-//#!/bin/bash
-//export INSTANCE_TYPE='m5.2xlarge'
-//export SG_IN='sg-0da9d4305febad027'
-//export SG_OUT='sg-037a189b3fb986cab'
+##!/bin/bash
+#export INSTANCE_TYPE='m5.2xlarge'
+#export SG_IN='sg-0da9d4305febad027'
+#export SG_OUT='sg-037a189b3fb986cab'
